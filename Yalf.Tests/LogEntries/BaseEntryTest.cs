ca839c8f9eba84de6f7ae5c07d4d01c9a4f719bb
sharpmonkey <gregor.pacnik@gmail.com>
2013-09-29 22:20:42 +1000

    update protobuf-net and add BaseEntry tests

---
+++ Yalf.Tests/LogEntries/BaseEntryTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using Yalf.LogEntries;
+
+namespace Yalf.Tests.LogEntries
+{
+    [TestFixture]
+    public class BaseEntryTest : ProtobufTest<BaseEntry>
+    {      
+        [Test]
+        public void LogEvent_Rountrip()
+        {
+            // arrange
+            var entry = new LogEvent(LogLevel.Info, DateTime.Now, "message");
+
+            // act
+            var deserialized = SerializationRoundtrip(entry);
+
+            // assert
+            Assert.IsNotNull(deserialized);
+        }
+
+        [Test]
+        public void MethodEntry_Rountrip()
+        {
+            // arrange
+            var entry = new MethodEntry(1, "method", null, DateTime.Now);
+
+            // act
+            var deserialized = SerializationRoundtrip(entry);
+
+            // assert
+            Assert.IsNotNull(deserialized);
+        }
+
+        [Test]
+        public void MethodExit_Rountrip()
+        {
+            // arrange
+            var entry = new MethodExit(2, "method", 11, true, "value");
+
+            // act
+            var deserialized = SerializationRoundtrip(entry);
+
+            // assert
+            Assert.IsNotNull(deserialized);
+        }
+
+        [Test]
+        public void ThreadData_Rountrip()
+        {
+            // arrange
+            var entry = new ThreadData(1, "tread name", new BaseEntry[] { new LogEvent(LogLevel.Info, DateTime.Now, "message") });
+
+            // act
+            var deserialized = SerializationRoundtrip(entry);
+
+            // assert
+            Assert.IsNotNull(deserialized);
+        }
+
+        [Test]
+        public void ExceptionTrace_Rountrip()
+        {
+            // arrange
+            var entry = new ExceptionTrace(new Exception("test"), DateTime.Now);
+
+            // act
+            var deserialized = SerializationRoundtrip(entry);
+
+            // assert
+            Assert.IsNotNull(deserialized);
+        }
+    }
+}
---
+++ Yalf.Tests/LogEntries/ProtobufTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using ProtoBuf;
+
+namespace Yalf.Tests.LogEntries
+{
+    [TestFixture]
+    public abstract class ProtobufTest<T> where T : class
+    {
+        private HashSet<string> _descendants = null;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var baseType = typeof(T);
+
+            _descendants = new HashSet<string>();
+
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()).Where(t => t.IsSubclassOf(baseType));
+            foreach (var type in types)
+            {
+                var typeName = type.Name;
+
+                var constructors = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+                if (constructors.All(c => c.GetParameters().Length != 0))
+                    throw new InvalidProgramException(string.Format("Type '{0}' missing default (empty) constructor", typeName));
+
+                _descendants.Add(typeName);
+            }
+
+            if (!_descendants.Any())
+                throw new InvalidOperationException(string.Format("Could not find any {0} descendants", baseType.Name));
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownAndCheckForMissingTests()
+        {
+            if (_descendants.Any())
+            {
+                throw new MissingMethodException(string.Format("Missing tests for: {0}", string.Join(", ", _descendants)));
+            }
+        }
+
+        protected U SerializationRoundtrip<U>(U value) where U : T
+        {
+            byte[] bytes = null;
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(ms, value);
+                bytes = ms.ToArray();
+            }
+
+            T deserialized = null;
+            using (var ms = new MemoryStream(bytes))
+            {
+                deserialized = Serializer.Deserialize<T>(ms);
+            }
+
+            _descendants.Remove(typeof(U).Name);
+
+            return (U)deserialized;
+        }
+    }
+}
