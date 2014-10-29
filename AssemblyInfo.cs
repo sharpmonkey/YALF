c02735c682e3d54c310182ba8428e348a6a4b4a6
sharpmonkey <gregor.pacnik@gmail.com>
2014-10-29 23:19:45 +1100

    NuGet release 0.6.8.1
    fix for nupkg folders (pepita)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,7 +8,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ObjectTree")]
 [assembly: AssemblyProduct("Yalf")]
-[assembly: AssemblyCopyright("Copyright © ObjectTree 2013")]
+[assembly: AssemblyCopyright("Copyright © Gregor Pacnik, Matthew Langmaid 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,5 +22,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.8.0")]
+[assembly: AssemblyVersion("0.6.8.1")]
 
